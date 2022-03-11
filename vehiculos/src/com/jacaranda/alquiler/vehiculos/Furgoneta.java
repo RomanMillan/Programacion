fefbd353b00516e446b40c864d7e4d82603b9b30
RomanMillan <romandos962m@gmail.com>
2022-03-11 09:57:59 +0100

    avance furgoneta

--- vehiculos/src/com/jacaranda/alquiler/vehiculos/Furgoneta.java
+++ vehiculos/src/com/jacaranda/alquiler/vehiculos/Furgoneta.java
@@ -1,8 +1,42 @@
 package com.jacaranda.alquiler.vehiculos;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class Furgoneta extends Vehiculo{
 
-	private static final double PMA = 5000;
-	private double precioPeso;
+	private double pMA;
+	private final double PRECIO_PESO = 0.5;
+	
+	public Furgoneta(String matricula, String gama, LocalDate fechaSalida, double pMA) throws VehiculoException {
+		super(matricula, gama, fechaSalida);
+		this.pMA = pMA;
+	}
+
+	public double getpMA() {
+		return pMA;
+	}
+
+	public void setpMA(double pMA) {
+		this.pMA = pMA;
+	}
+
+	public double getPRECIO_PESO() {
+		return PRECIO_PESO;
+	}
+	//TODO TERMINAR ESTO
+	/*
+	@Override
+	public double getPrecio() {
+		return super.getPrecio()  + pMA * PRECIO_PESO
+				this.getFechaEntrada().until(getFechaSalida(),ChronoUnit.DAYS);
+	}
+	*/
+	@Override
+	public String toString() {
+		return "Furgoneta [pMA=" + pMA + ", PRECIO_PESO=" + PRECIO_PESO + "]";
+	}
+	
+	
 	
 }
